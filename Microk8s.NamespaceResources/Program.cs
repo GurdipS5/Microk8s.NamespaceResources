d6b3b3745f5b5d1abbdb17e405f1152de25742ab
GurdipS5 <gurdip.sira@gmail.com>
2024-04-02 02:43:46 +0100

    feat: :sparkles: code
    code
    
    BREAKING CHANGE: code

---
+++ Microk8s.NamespaceResources/Program.cs
@@ -0,0 +1,62 @@
+﻿using Pulumi;
+using Pulumi.Kubernetes.Types.Inputs.Core.V1;
+using Pulumi.Kubernetes.Types.Inputs.Apps.V1;
+using Pulumi.Kubernetes.Types.Inputs.Meta.V1;
+using System.Collections.Generic;
+
+return await Deployment.RunAsync(() =>
+{
+    var appLabels = new InputMap<string>
+    {
+        { "app", "nginx" }
+    };
+
+    var namespaces = new Pulumi.Kubernetes.Core.V1.NamespaceList("");
+
+
+
+    var deployment = new Pulumi.Kubernetes.Core.V1.NamespaceList("nginx", new DeploymentArgs
+    {
+
+
+        Spec = new DeploymentSpecArgs
+        {
+            Selector = new LabelSelectorArgs
+            {
+                MatchLabels = appLabels
+            },
+            Replicas = 1,
+            Template = new PodTemplateSpecArgs
+            {
+                Metadata = new ObjectMetaArgs
+                {
+                    Labels = appLabels
+                },
+                Spec = new PodSpecArgs
+                {
+                    Containers =
+                    {
+                        new ContainerArgs
+                        {
+                            Name = "nginx",
+                            Image = "nginx",
+                            Ports =
+                            {
+                                new ContainerPortArgs
+                                {
+                                    ContainerPortValue = 80
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    });
+
+    // export the deployment name
+    return new Dictionary<string, object?>
+    {
+        ["name"] =  deployment.Metadata.Apply(m => m.Name)
+    };
+});
