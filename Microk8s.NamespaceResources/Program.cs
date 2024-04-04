587a5aef5792635a54a083ebd4ed9ee92ccb5e18
GurdipS5 <gurdip.sira@gmail.com>
2024-04-04 01:34:30 +0100

    feat: :sparkles: namespace resources
    na
    
    BREAKING CHANGE: na

--- Microk8s.NamespaceResources/Program.cs
+++ Microk8s.NamespaceResources/Program.cs
@@ -13,50 +13,11 @@ return await Deployment.RunAsync(() =>
 
     var namespaces = new Pulumi.Kubernetes.Core.V1.NamespaceList("");
 
-
-
-    var deployment = new Pulumi.Kubernetes.Core.V1.NamespaceList("nginx", new DeploymentArgs
-    {
-
-
-        Spec = new DeploymentSpecArgs
-        {
-            Selector = new LabelSelectorArgs
-            {
-                MatchLabels = appLabels
-            },
-            Replicas = 1,
-            Template = new PodTemplateSpecArgs
-            {
-                Metadata = new ObjectMetaArgs
-                {
-                    Labels = appLabels
-                },
-                Spec = new PodSpecArgs
-                {
-                    Containers =
-                    {
-                        new ContainerArgs
-                        {
-                            Name = "nginx",
-                            Image = "nginx",
-                            Ports =
-                            {
-                                new ContainerPortArgs
-                                {
-                                    ContainerPortValue = 80
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-        }
-    });
+    namespaces.
 
     // export the deployment name
     return new Dictionary<string, object?>
     {
-        ["name"] =  deployment.Metadata.Apply(m => m.Name)
+        //    ["name"] =  deployment.Metadata.Apply(m => m.Name)
     };
 });
--- build/Build.cs
+++ build/Build.cs
@@ -19,6 +19,177 @@ class Build : NukeBuild
     ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
     ///   - Microsoft VSCode           https://nuke.build/vscode
 
+
+    /// <summary>
+    ///
+    /// </summary>
+    public string CodeCoverage = "true";
+
+    /// <summary>
+    ///
+    /// </summary>
+    public string Space = " ";
+
+    #region NetCoreBuild
+
+    public string framework = "net8.0";
+    string SelfContained = string.Empty;
+    string runtime = "win-x64";
+
+    #endregion
+
+    #region Secrets
+
+    /// <summary>
+    ///  ProGet server key.
+    /// </summary>
+    [Parameter][Secret] readonly string NuGetApiKey;
+
+    /// <summary>
+    /// Codecov test token.
+    /// </summary>
+    [Secret][Parameter] readonly string CODECOV_SECRET;
+
+    /// <summary>
+    /// Sonarqube API Key.
+    /// </summary>
+    [Secret][Parameter] readonly string SonarKey;
+
+    /// <summary>
+    /// GitHub PAT token.
+    /// </summary>
+    [Secret][Parameter] readonly string GitHubToken;
+
+    /// <summary>
+    /// Sny API Token.
+    /// </summary>
+    [Secret][Parameter] readonly string SNYK_TOKEN;
+
+    /// <summary>
+    ///     License key for Report Generator.
+    /// </summary>
+    [Parameter][Secret] readonly string ReportGeneratorLicense;
+
+    /// <summary>
+    ///     Dependency Track API Key.
+    /// </summary>
+    [Parameter][Secret] readonly string DTrackApiKey2;
+
+    #endregion
+
+
+    #region Paths
+
+    /// <summary>
+    ///
+    /// </summary>
+    readonly AbsolutePath UtilsDir = RootDirectory / "Utilities";
+
+    /// <summary>
+    /// s.
+    /// </summary>
+    readonly AbsolutePath BuildDir = RootDirectory / "Nuke" / "Output" / "Build";
+
+    /// <summary>
+    /// Codecov config file.
+    /// </summary>
+    readonly AbsolutePath CodecovYml = RootDirectory / "codecov.yml";
+
+    /// <summary>
+    /// Artifacts directory.
+    /// </summary>
+    readonly AbsolutePath Artifacts = RootDirectory / "Nuke" / "Artifacts";
+
+    /// <summary>
+    ///  Output of coverlet code  coverage report.
+    /// </summary>
+    readonly AbsolutePath QodanaOut = RootDirectory / "Nuke" / "Qodana" / "Results";
+
+    /// <summary>
+    ///  Output of coverlet code  coverage report.
+    /// </summary>
+    readonly AbsolutePath QodanaReport = RootDirectory / "Nuke" / "Qodana" / "Report";
+
+    /// <summary>
+    ///  Output of coverlet code  coverage report.
+    /// </summary>
+    readonly AbsolutePath QodanaCache = @"D:\QodanaCache";
+
+    /// <summary>
+    ///  Output of coverlet code  coverage report.
+    /// </summary>
+    readonly AbsolutePath CoverletOutput = RootDirectory / "Nuke" / "Output" / "Coverlet";
+
+    /// <summary>
+    /// NDependOutput folder.
+    /// </summary>
+    readonly AbsolutePath NukeOut = RootDirectory / "Nuke";
+
+    /// <summary>
+    ///  NDependOutput folder.
+    /// </summary>
+    readonly AbsolutePath NDependOutput = RootDirectory / "Nuke" / "Output" / "NDependOut";
+
+    /// <summary>
+    /// GGShield config file.
+    /// </summary>
+    readonly AbsolutePath GgConfig = RootDirectory / "gitguardian.yml";
+
+    /// <summary>
+    /// Dotnet publish output directory
+    /// </summary>
+    readonly AbsolutePath PublishFolder = RootDirectory / "Nuke" / "Output" / "Publish";
+
+    /// <summary>
+    /// PVS Studio log output folder.
+    /// </summary>
+    readonly AbsolutePath PvsStudio = RootDirectory / "Nuke" / "Output" / "PVS";
+
+    /// <summary>
+    ///     Path to nupkg file from the project
+    /// </summary>
+    readonly AbsolutePath NupkgPath = RootDirectory / "Nuke" / "Output" / "Nuget";
+
+    /// <summary>
+    /// Coverlet report folder.
+    /// </summary>
+    readonly AbsolutePath ReportOut = RootDirectory / "Nuke" / "Output" / "Coverlet" / "Report";
+
+    /// <summary>
+    ///  Output directory of the SBOM file from CycloneDX
+    /// </summary>
+    readonly AbsolutePath Sbom = RootDirectory / "Nuke" / "Output" / "SBOM";
+
+
+    /// <summary>
+    /// Filename of changelog file.
+    /// </summary>
+    string ChangeLogFile => RootDirectory / "changelog.md";
+
+    /// <summary>
+    ///  Docfx folder.
+    /// </summary>
+
+    AbsolutePath DocFxLibrary => RootDirectory / "docfx_project";
+
+    /// <summary>
+    /// Directory of MSTests project.
+    /// </summary>
+    AbsolutePath TestsDirectory => RootDirectory / "Tests";
+
+    /// <summary>
+    /// Directory of MSTests project.
+    /// </summary>
+    AbsolutePath CoverletSrc => RootDirectory / "Tests" / "unittestresults";
+
+
+    /// <summary>
+    /// Target path.
+    /// </summary>
+    readonly AbsolutePath TargetPath = RootDirectory / "Nuke" / "Output" / "Coverlet" / "Report";
+
+
+
     public static int Main () => Execute<Build>(x => x.Compile);
 
     [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
